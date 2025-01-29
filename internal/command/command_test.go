ec492cac6a6f9631a270169898da805b9ebf65c0
leonardovee <leoveeph@gmail.com>
2025-01-29 12:13:02 -0300

    wait for the coroutines to end

--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -12,16 +12,21 @@ func TestCommand_processCommands(t *testing.T) {
 
 	t.Run("should process commands", func(t *testing.T) {
 		dispatcher := NewCommandDispatcher()
+		done := make(chan struct{}, 1)
 
 		command := NewMockCommand(ctrl)
 		command.EXPECT().GetName().AnyTimes().Return(CommandType("test"))
 
 		commandHandler := NewMockCommandHandler(ctrl)
 		commandHandler.EXPECT().GetName().AnyTimes().Return(CommandType("test"))
-		commandHandler.EXPECT().Handle(command).Return(nil)
+		commandHandler.EXPECT().Handle(command).DoAndReturn(func(cmd Command) error {
+			defer close(done)
+			return nil
+		})
 
 		RegisterHandler(dispatcher, commandHandler)
 
 		dispatcher.Dispatch(command)
+		<-done
 	})
 }
