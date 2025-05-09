591b9ac20e11b632c0ccb362fa27f0316e7cb7c9
leonardovee <leoveeph@gmail.com>
2025-05-09 19:45:48 +0000

    idk

--- README.md
+++
@@ -1,9 +0,0 @@
-## leonardovee.dev/command
-
-a simple command handler framework
-
-## todo
-
-- [] segregate the framework part from the userland
-- [] create a /example folder containing the userland app
-- [] containers and benchmarking
--- example/scripts/localstack/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-awslocal sns create-topic --name command-events 
-awslocal sqs create-queue --queue-name command-events-queue
-awslocal sns subscribe \
-    --topic-arn arn:aws:sns:us-east-1:000000000000:command-events \
-    --protocol sqs \
-    --notification-endpoint arn:aws:sqs:us-east-1:000000000000:command-events-queue
